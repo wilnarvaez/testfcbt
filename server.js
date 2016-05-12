a3a1a1c00bf203759f7dd04151269211725002dd
Wilmer Narváez <wilnarvaez@gmail.com>
2016-05-11 23:24:49 -0500

    fix menor

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var Botkit = required('botkit')
 
 var accessToken = process.env.FACEBOOK_PAGE_ACCESS_TOKEN
 var verifyToken = process.env.FACEBOOK_VERIFY_TOKEN
-var port = proess.env.port
+var port = process.env.port
 
 if (!accessToken) throw new Error('FACEBOOK_PAGE_ACCESS_TOKEN is required but missing')
 if (!verifyToken) throw new Error('FACEBOOK_VERIFY_TOKEN is required but missing')
