ae6959375653fda7f862ba3c72db1ba7f89c515c
Wilmer Narváez <wilnarvaez@gmail.com>
2016-05-21 14:09:05 -0500

    reorganizado

--- server.js
+++ server.js
@@ -104,31 +104,27 @@ controller.on('facebook_postback', function (bot, message) {
       if (!error && response.statusCode == 200) {
         var jsonData = JSON.parse(body);
         for (var i = 0; i < jsonData.mascotas.length; i++) {
-            var counter = jsonData.mascotas[i];
-      bot.reply(message, {
-        attachment: {
-          type: 'template',
-          payload: {
-            template_type: 'generic',
-            elements: [{
-                      title: counter.mta_name,
-                      subtitle: 'Cuido Mi Mascota',
-                      image_url: 'http://www.cuidomimascota.com/pictures/'+counter.path,
-                      buttons: [{
-                        type: 'web_url',
-                        url: 'http://www.cuidomimascota.com/perfil/'+counter.id,
-                        title: counter.mta_name
-                      }, {
-                        type: 'postback',
-                        title: 'Postback',
-                        payload: 'Payload for first element in a generic bubble',
-                      }],
-                    }],
-                  }
-                }
-              })
-          }
+          var counter = jsonData.mascotas[i];
+          bot.reply(message, {
+            attachment: {
+              type: 'template',
+              payload: {
+                template_type: 'generic',
+                elements: [{
+                          title: counter.mta_name,
+                          subtitle: 'Cuido Mi Mascota',
+                          image_url: 'http://www.cuidomimascota.com/pictures/'+counter.path,
+                          buttons: [{
+                            type: 'web_url',
+                            url: 'http://www.cuidomimascota.com/perfil/'+counter.id,
+                            title: counter.mta_name
+                          }],
+                }],
+              }
+            }
+          })
         }
-      })
+      }
+    })
   }
 })
