ab5be56e47c504ae2354d4b988a271723f4a8602
Wilmer Narvaez <wilnarvaez@gmail.com>
2016-06-10 18:46:46 -0500

    fix 3

--- server.js
+++ server.js
@@ -69,14 +69,3 @@ controller.on('facebook_postback', function (bot, message) {
       break
   }
 })
-
-//****************************************
-//             segunda parte             *
-//****************************************
-controller.hears(['@gato', '@gatos'], 'message_received', function (bot, message) {
-  bot.reply(message, 'Mira estos:')
-})
-
-controller.on('facebook_postback', function (bot, message) {
-  mensaje('http://cuidomimascota.com/botgato')
-})
